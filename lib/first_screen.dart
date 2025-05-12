d40d1c72051a7a55280fc2932d108d801afefb9d
simonedibiase <s.dibiase4@studenti.unimol.it>
2025-05-12 16:59:42 +0200

    Created screen: FirstScreen

---
+++ lib/first_screen.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+                SizedBox(
+                width: double.infinity,
+                height: MediaQuery.of(context).size.height / 1.7,
+                child: Image.asset(
+                  'assets/negroni4.jpg',
+                  fit: BoxFit.contain, 
+                ),
+              ),
+              Text(
+                'It’s the cocktails\no’clock',
+                style: GoogleFonts.poppins(
+                  color: Colors.white,
+                  fontSize: 32,
+                  height: 1.2,
+                  fontWeight: FontWeight.w900
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 40),
+              Text(
+                'Discover refined recipes based on \nyour available ingredients',
+                style: GoogleFonts.poppins(
+                  color: Colors.white,
+                  fontSize: 13,
+                  height: 1.2,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 40),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(255, 255, 106, 0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  padding: const EdgeInsets.symmetric(horizontal: 75, vertical: 10),
+                ),
+                onPressed: () {}, 
+                child: Text(
+                  'Start',
+                  style: GoogleFonts.poppins(
+                    color: Colors.white,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
