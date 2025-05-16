b56b1b9c048ada390ba00bc44ff7c4b7303d66f6
simonedibiase <s.dibiase4@studenti.unimol.it>
2025-05-16 11:31:22 +0200

    Update themes in first_screen.dart

--- lib/screen/first_screen.dart
+++ lib/screen/first_screen.dart
@@ -6,62 +6,58 @@ class FirstScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.black,
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-                SizedBox(
-                width: double.infinity,
-                height: MediaQuery.of(context).size.height / 1.7,
-                child: Image.asset(
-                  'assets/negroni4.jpg',
-                  fit: BoxFit.contain, 
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: double.infinity,
+              height: MediaQuery.of(context).size.height / 1.7,
+              child: Image.asset('assets/negroni4.jpg', fit: BoxFit.contain),
+            ),
+            Text(
+              'It’s the cocktails\no’clock',
+              style: Theme.of(
+                context,
+              ).textTheme.bodyLarge?.copyWith(color: Colors.white, height: 1.2),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 40),
+            Text(
+              'Discover refined recipes based on \nyour available ingredients',
+              style: GoogleFonts.poppins(
+                color: Colors.white,
+                fontSize: 13,
+                height: 1.2,
+                fontWeight: FontWeight.normal,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 40),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color.fromARGB(255, 255, 106, 0),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 75,
+                  vertical: 10,
                 ),
               ),
-              Text(
-                'It’s the cocktails\no’clock',
+              onPressed: () {},
+              child: Text(
+                'Start',
                 style: GoogleFonts.poppins(
                   color: Colors.white,
-                  fontSize: 32,
-                  height: 1.2,
-                  fontWeight: FontWeight.w900
-                ),
-                textAlign: TextAlign.center,
-              ),
-              const SizedBox(height: 40),
-              Text(
-                'Discover refined recipes based on \nyour available ingredients',
-                style: GoogleFonts.poppins(
-                  color: Colors.white,
-                  fontSize: 13,
-                  height: 1.2,
-                ),
-                textAlign: TextAlign.center,
-              ),
-              const SizedBox(height: 40),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color.fromARGB(255, 255, 106, 0),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                  padding: const EdgeInsets.symmetric(horizontal: 75, vertical: 10),
-                ),
-                onPressed: () {}, 
-                child: Text(
-                  'Start',
-                  style: GoogleFonts.poppins(
-                    color: Colors.white,
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                  ),
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
