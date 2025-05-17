7031fecd38daddf8b56f9f0f1eb634a228f8c487
simonedibiase <s.dibiase4@studenti.unimol.it>
2025-05-17 08:46:09 +0200

    Created MainScreen widget to manage BottomNavigationBar

---
+++ lib/navigation/main_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:my_coctails_bar/screen/cocktails.dart';
+import 'package:my_coctails_bar/screen/ingredients.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int currentPageIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        bottomNavigationBar: NavigationBar(
+          onDestinationSelected: (int index) {
+            setState(() {
+              currentPageIndex = index;
+            });
+          },
+          height: MediaQuery.of(context).size.height * 0.075,
+          selectedIndex: currentPageIndex,
+          destinations: const [
+            NavigationDestination(
+              icon: Icon(Icons.local_bar),
+              label: 'Cocktail',
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.list_alt),
+              label: 'Ingredients',
+            ),
+          ],
+        ),
+        body: [Cocktails(), Ingredients()][currentPageIndex],
+    );
+  }
+}
