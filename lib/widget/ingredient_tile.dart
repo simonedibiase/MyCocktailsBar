451f8eff32d1dbba1e5373357995959f49f1ddaf
simonedibiase <s.dibiase4@studenti.unimol.it>
2025-05-24 18:50:26 +0200

    Created IngredientTile widget to display ingredients list

---
+++ lib/widget/ingredient_tile.dart
@@ -0,0 +1,30 @@
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter/material.dart';
+import 'package:my_coctails_bar/models/ingredient.dart';
+
+class IngredientTile extends StatelessWidget {
+  final Ingredient ingredient;
+  final VoidCallback onDelete;
+
+  const IngredientTile(this.ingredient, {required this.onDelete, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Image.network(ingredient.imageUrl, width: 70, height: 70),
+      title: Text(
+        ingredient.nome,
+        maxLines: 1,
+        overflow: TextOverflow.ellipsis,
+        style: TextStyle(fontSize: 18),
+      ),
+      trailing: IconButton(
+        icon: const Icon(
+          Icons.delete,
+          color: Color.fromARGB(255, 196, 196, 196),
+        ),
+        onPressed: onDelete,
+      ),
+    );
+  }
+}
