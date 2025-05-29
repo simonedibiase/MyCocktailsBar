abf20c17e7bcb234fb5729bf3a8d35b0de4e296e
simonedibiase <s.dibiase4@studenti.unimol.it>
2025-05-29 11:38:59 +0200

    added categories to Cocktails screen

--- lib/screen/cocktails.dart
+++ lib/screen/cocktails.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:my_coctails_bar/widget/category_card.dart';
 import 'package:my_coctails_bar/widget/my_search_bar.dart';
 
 class Cocktails extends StatefulWidget {
@@ -19,16 +20,27 @@ class _CocktailsState extends State<Cocktails> {
       body: Column(
         children: [
           MySearchBar(),
-          Container(
-            width: screenWidth * 0.9,
-            alignment: Alignment.centerLeft,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Text(
-                  'Available cocktails',
-                  style: Theme.of(context).textTheme.bodyMedium,
-                ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.08),
+            child: Container(
+              width: double.infinity,
+              child: Text(
+                'Select the category',
+                style: Theme.of(context).textTheme.bodyMedium,
+                textAlign: TextAlign.left,
+              ),
+            ),
+          ),
+
+          Expanded(
+            child: GridView.count(
+              crossAxisCount: 2,
+              physics: NeverScrollableScrollPhysics(), // Disabilita lo scroll
+              children: const [
+                CategoryCard(image: 'dolce.jpeg', label: 'Sweet cocktail'),
+                CategoryCard(image: 'amaro.jpg', label: 'bitter cocktail'),
+                CategoryCard(image: 'shot.jpg', label: 'Shot'),
+                CategoryCard(image: 'analcolico.jpg', label: 'mocktail'),
               ],
             ),
           ),
