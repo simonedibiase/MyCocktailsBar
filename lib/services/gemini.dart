92d89f9cccd7a4bf98f4f957db3952aff5ffa8fb
simonedibiase <s.dibiase4@studenti.unimol.it>
2025-06-01 11:25:21 +0200

    Added methods to generate cocktails using Gemini

--- lib/services/gemini.dart
+++ lib/services/gemini.dart
@@ -136,11 +136,6 @@ class Gemini {
                 description: "Nome dell'ingrediente",
                 nullable: true,
               ),
-              /*"url": Schema(
-                SchemaType.string,
-                description: "URL foto dell'ingrediente",
-                nullable: true,
-              ),*/
             },
           ),
         ),
@@ -153,7 +148,35 @@ class Gemini {
     );
   }
 
-  Future<String> getCocktail(String ingredients, String category) async {
+  Future getCocktail(String ingredients, String category) async {
+    Gemini gemini = Gemini();
+    updateSchema(_setSchemaCocktail());
+    GenerativeModel model = gemini.model;
+
+    print('INGREDINTI DISPONIBILI: *$ingredients**');
+
+    var prompt =
+        '''Sei uno barman esperto di alcolici che ha viaggiato per il mondo e che conosce tutti i coktail tipici di ogni paese.\n
+            ora gestisci un bar con clienti molto esigenti, devi rispettare esattamente le richieste dei clienti.
+            Voglio che tu suggerisca un cocktail da preparare, nello specifico devi creare un $category, controlla quindi che il coktail rispetta esattamente tale categoria\n\n
+            gli ingredienti che hai a disposizione sono i seguenti: $ingredients, non devi usare altri ingredienti oltre quelli a disposizione, se hai a disposizione un ingrediente non puoi usare una sua variante, ad esempio se hai a disposizione acqua non puoi usare acqua frizzante.
+            La prima cosa che devi fare è controllare che gli ingredienti che ho a disposizione sono sufficienti per la preparazione, se non ho ingredienti o se non sono sufficienti\n
+            segnalalo con un messaggio di errore specificano che gli ingredienti non sono sufficienti all' interno del campo Error, e non generarmi un cocktail e restituiscimi null per il resto delle informazioni.\n\n
+            
+            Altrimenti, solo se i miei ingredienti sono sufficienti, Voglio che tu mi dia il titolo di un coktail , gli ingredienti e la ricetta per la reparazione del cocktail. Se hai trovato aleno un'errore nei dati allora descrivimi l'errore, altrimenti
+            imposta l'errore a null''';
+
+    final content = [Content.text(prompt)];
+    final response = await model.generateContent(content);
+
+    return response;
+  }
+
+  Future getNewCocktail(
+    String ingredients,
+    String category,
+    var oldCocktails,
+  ) async {
     Gemini gemini = Gemini();
     updateSchema(_setSchemaCocktail());
     GenerativeModel model = gemini.model;
@@ -161,17 +184,19 @@ class Gemini {
 
     var prompt =
         '''Sei uno barman esperto di alcolici che ha viaggiato per il mondo e che conosce tutti i coktail tipici di ogni paese.\n
-            Voglio che tu suggerisca un cocktail da preparare, nello specifico devi creare un $category.\n\n
-            gli ingredienti che hai a disposizione sono i seguenti: $ingredients, non devi usare altri ingredienti oltre quelli a disposizione.
-            La prima cosa che devi fare è controllare che gli ingredienti che ho a disposizione sono sufficienti per la preparazione, se non ho ingredienti o se non sono sufficienti\n
+            ora gestisci un bar con clienti molto esigenti, devi rispettare esattamente le richieste dei clienti.
+            Voglio che tu suggerisca un cocktail da preparare, nello specifico devi creare un $category, controlla quindi che il coktail rispetta esattamente tale categoria.\n\n
+            hai già preparato i seguenti coktail: $oldCocktails.
+            gli ingredienti che hai a disposizione sono i seguenti: $ingredients, non devi usare altri ingredienti oltre quelli a disposizione, se hai a disposizione un ingrediente non puoi usare una sua variante, ad esempio se hai a disposizione acqua non puoi usare acqua frizzante.
+            La prima cosa che devi fare è controllare che gli ingredienti che ho a disposizione sono sufficienti per la preparazione di un coktail diverso da quelli che hai già preparato, se gli ingredienti non sono sufficienti per la preparazione di un nuovo coktail\n
             segnalalo con un messaggio di errore specificano che gli ingredienti non sono sufficienti all' interno del campo Error, e non generarmi un cocktail e restituiscimi null per il resto delle informazioni.\n\n
             
-            Altrimenti, solo se i miei ingredienti sono sufficienti, Voglio che tu mi dia il titolo di un coktail , gli ingredienti con i rispettivi url delle foto e la ricetta per la reparazione del cocktail. Se hai trovato aleno un'errore nei dati allora descrivimi l'errore, altrimenti
+            Altrimenti, solo se i miei ingredienti sono sufficienti per la preparazione di un coktail diverso dai precedenti,, Voglio che tu mi dia il titolo di un coktail , gli ingredienti con i rispettivi url delle foto e la ricetta per la reparazione del cocktail. Se hai trovato aleno un'errore nei dati allora descrivimi l'errore, altrimenti
             imposta l'errore a null''';
 
     final content = [Content.text(prompt)];
     final response = await model.generateContent(content);
 
-    return response.text!;
+    return response;
   }
 }
