6a70d2f4cc22c1a2c521f8545e9a291ad5ac0a00
simonedibiase <s.dibiase4@studenti.unimol.it>
2025-06-01 11:21:04 +0200

    Add fromMap method to Ingredient class

--- lib/models/ingredient.dart
+++ lib/models/ingredient.dart
@@ -6,6 +6,14 @@ class Ingredient {
   Ingredient({required this.id, required this.nome, required this.imageUrl});
 
   Map<String, Object?> toMap() {
-    return {'id': id, 'nome': nome, 'url': imageUrl};
+    return {'id': id, 'nome': nome.toLowerCase().trim(), 'url': imageUrl};
+  }
+
+  static Ingredient fromMap(Map<String, dynamic> map) {
+    return Ingredient(
+      id: map['id'] as int,
+      nome: map['nome'] as String,
+      imageUrl: map['url'] as String,
+    );
   }
 }
